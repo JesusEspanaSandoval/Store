1ed79735e9ffa2c4191e819ab2448068e4105b81
Jesús España Sandoval <jesusitoJ1@hotmail.com>
2023-01-15 07:51:25 -0500

    Create a product

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,14 @@
                 </li>
               @endif
             @else
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('home') }}">Home</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('products.create') }}">Create a product</a>
+              </li>
+
               <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                   data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
