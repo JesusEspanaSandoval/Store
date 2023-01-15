05d605ab2490ef5bd4853475ebbf66a26a0c89b6
Jesús España Sandoval <jesusitoJ1@hotmail.com>
2023-01-15 12:30:03 -0500

    Fix details

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,15 @@
 @section('content')
   <div class="container">
     <div>
-      <h1 class="text-center fw-bold">Your last products</h1>
+      @if (count($products) != 0)
+        <h1 class="text-center fw-bold">Your last products</h1>
+      @endif
       <div class="d-flex justify-content-center flex-wrap mt-2">
         @forelse ($products as $product)
           <div class="card m-2" style="width: 18rem;">
             <img src="storage/{{ $product->product_picture }}" class="card-img-top">
             <div class="card-body">
-              <h5 class="card-title">{{ $product->name }} {{ $product->price }}$</h5>
+              <h5 class="card-title">{{ $product->name }} ${{ $product->price }}</h5>
               <p class="card-text">{{ $product->description }}</p>
               <form method="post" action="{{ route('products.destroy', $product->id) }}"
                 class="d-flex align-items-center">
@@ -21,9 +23,9 @@
             </div>
           </div>
         @empty
-          <div class="d-flex flex-col align-items-center card mt-2" style="width: 18rem;">
-            <h1 class="text-center">You don't have products</h1>
-            <a href="{{ route('register') }}" class="btn btn-primary">Create a product</a>
+          <div class="d-flex flex-col align-items-center card mt-2 p-4">
+            <h3 class="text-center">You don't have products</h3>
+            <a href="{{ route('products.create') }}" class="btn btn-primary">Create a product</a>
           </div>
         @endforelse
       </div>
